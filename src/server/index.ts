eb0cd94c27ce3dd0a1dddde04016f49f10f22ca3
Edimaobong Matthew <2sweetworld@gmail.com>
2021-11-23 11:27:40 +0200

    Formatting fixes

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,50 +3,48 @@ var cors = require('cors')
 const app = express()
 app.use(express.json())
 const port = 4000
- 
+
 app.use(cors())
 
 let data = {}
 let lastId= 0;
 
 app.get('/phonebook', (req, res) => {
-    res.json(data)
+  res.json(data)
 })
 
 app.post('/phonebook', (req, res) => {
-    lastId++;
-
-    data[lastId] = {
-        id: lastId,
-        phoneNumber: req.body.phoneNumber,
-        emailAddress: req.body.emailAddress,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
-    }
-
-    res.json(data)
+  lastId++;
+  data[lastId] = {
+    id: lastId,
+    phoneNumber: req.body.phoneNumber,
+    emailAddress: req.body.emailAddress,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+  }
+  res.json(data)
 })
 
 app.put('/phonebook/:id', (req, res) => {
-    if(!req.params.id && !(req.params.id in data)){
-        res.send('Invalid Id')
-    }
-    data[req.params.id] = {
-        id: req.params.id,
-        phoneNumber: req.body.phoneNumber,
-        emailAddress: req.body.emailAddress,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
-    }
-    res.json(data)
+  if(!req.params.id && !(req.params.id in data)){
+    res.send('Invalid Id')
+  }
+  data[req.params.id] = {
+    id: req.params.id,
+    phoneNumber: req.body.phoneNumber,
+    emailAddress: req.body.emailAddress,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+  }
+  res.json(data)
 })
 
 app.delete('/phonebook/:id', (req, res) => {
-    if(!req.params.id && !(req.params.id in data)){
-        res.send('Invalid Id')
-    }
-    delete data[req.params.id]
-    res.json(data)
+  if(!req.params.id && !(req.params.id in data)){
+    res.send('Invalid Id')
+  }
+  delete data[req.params.id]
+  res.json(data)
 })
 
 app.listen(port, () => {
--- src/store.ts
+++ src/store.ts
@@ -13,21 +13,26 @@ export default createStore({
     },
     showContact: false,
     errors: {}
-  }) ,
+  }),
+
   mutations: {
     toggleContactModal(state) {
       state.showContact = !state.showContact
     },
-    showContactModal(state){
+
+    showContactModal(state) {
       state.showContact = true;
     },
-    hideContactModal(state){
+
+    hideContactModal(state) {
       state.showContact = false;
     },
-    setContact(state,json){
+
+    setContact(state,json) {
       state.contacts = json
     },
-    clearContactForm(state, json){
+
+    clearContactForm(state, json) {
       state.contactData = {
         emailAddress: '',
         phoneNumber: '',
@@ -36,137 +41,151 @@ export default createStore({
         id: 0
       }
     },
-    setError(state,data){
+
+    setUpdateData(state, cc) {
+      state.contactData = {
+        emailAddress: cc.emailAddress,
+        phoneNumber: cc.phoneNumber,
+        firstName: cc.firstName,
+        lastName: cc.lastName,
+        id: cc.id
+      }
+    },
+
+    setError(state,data) {
       state.errors = data
     },
-    updatePhoneNumber(state, value){
+
+    updatePhoneNumber(state, value) {
       state.contactData.phoneNumber = value
     },
-    updateEmailAddress(state, value){
+
+    updateEmailAddress(state, value) {
       state.contactData.emailAddress = value
     },
-    updateFirstName(state, value){
+
+    updateFirstName(state, value) {
       state.contactData.firstName = value
     },
-    updateLastName(state, value){
+
+    updateLastName(state, value) {
       state.contactData.lastName = value
     },
-    updateContactId(state, value){
+
+    updateContactId(state, value) {
       state.contactData.id = value
     },
-    
   },
+
   getters: {
-    shouldShowContactModal(state){
+    shouldShowContactModal(state) {
       return state.showContact
     },
   },
+
   actions: {
     async getContactData (context) {
       let resp = await fetch(endpoint)
       let json = await resp.json()
-      context.commit("setContact",json)  
+      context.commit("setContact", json)  
     },
     async createContact (context) {
       let err = validate(context.state)
 
-      if(Object.keys(err).length>=1){
+      if(Object.keys(err).length>=1) {
         context.commit("setError",err)
         return false
       }
 
       try{
-      let resp = await fetch(endpoint, {
-        method: "POST",
-        body: JSON.stringify(context.state.contactData),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      let json = await resp.json()
-      context.commit("setContact",json)
-      context.dispatch("getContactData");
-      return true;
-    }catch(e){
-      this.$toast.open({
-        message: 'An error occured',
-        type: 'error'
-      });
-    }
-    return false
-  },
-  editContact(context, contactId){
-    let cc = context.state.contacts[contactId]
-    context.commit("setError",{})
-    if(cc){
-      context.commit("updateEmailAddress", cc.emailAddress)
-      context.commit("updatePhoneNumber", cc.phoneNumber)
-      context.commit("updateFirstName", cc.firstName)
-      context.commit("updateLastName", cc.lastName)
-      context.commit("updateContactId", cc.id)
-    }
-  },
-  async updateContact (context, contactId) {
-    let err = validate(context.state)
-    
-
-    if(Object.keys(err).length>=1){
-      context.commit("setError",err)
-      return false
-    }
-
-    let data = {
-      firstName: this.state.contactData.firstName,
-      lastName: this.state.contactData.lastName,
-      phoneNumber: this.state.contactData.phoneNumber,
-      emailAddress: this.state.contactData.emailAddress,
-    }
-    try{
-      let resp = await fetch(endpoint + contactId, {
-        method: "PUT",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      let json = await resp.json()
-      context.commit("setContact",json)
-      context.commit("clearContactForm")
-        return true
-      } catch (e) {
+        let resp = await fetch(endpoint, {
+          method: "POST",
+          body: JSON.stringify(context.state.contactData),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        let json = await resp.json()
+        context.commit("setContact", json)
+        context.dispatch("getContactData");
+        return true;
+      } catch(e) {
         this.$toast.open({
           message: 'An error occured',
           type: 'error'
         });
       }
+      return false
     },
-    async deleteContact (context, contactId) {
-      let resp = await fetch(endpoint + contactId, {
-        method: "DELETE",
-      })
-      let json = await resp.json()
-      context.commit("setContact",json)
+
+    editContact(context, contactId) {
+      let cc = context.state.contacts[contactId]
+      context.commit("setError", {})
+      if(cc) {
+        context.commit("setUpdateData", cc)
+      }
     },
-  },
-});
+
+    async updateContact (context, contactId) {
+      let err = validate(context.state)
+    
+      if(Object.keys(err).length>=1) {
+        context.commit("setError", err)
+        return false
+      }
+
+      let data = {
+        firstName: this.state.contactData.firstName,
+        lastName: this.state.contactData.lastName,
+        phoneNumber: this.state.contactData.phoneNumber,
+        emailAddress: this.state.contactData.emailAddress,
+      }
+      try{
+        let resp = await fetch(endpoint + contactId, {
+          method: "PUT",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        let json = await resp.json()
+        context.commit("setContact", json)
+        context.commit("clearContactForm")
+          return true
+        } catch (e) {
+          this.$toast.open({
+            message: 'An error occured',
+            type: 'error'
+          });
+        }
+      },
+      async deleteContact (context, contactId) {
+        let resp = await fetch(endpoint + contactId, {
+          method: "DELETE",
+        })
+        let json = await resp.json()
+        context.commit("setContact", json)
+      },
+    },
+  });
 
 
-function validate(state){
+function validate(state) {
   let email = state.contactData.emailAddress
   let phone = state.contactData.phoneNumber
   let firstName = state.contactData.firstName
   let lastName = state.contactData.lastName
   let ee: {[prop:string]:string} = {}
 
-  if(!email || !validateEmail(email)){
+  if(!email || !validateEmail(email)) {
     ee.email = "Invalid Email Supplied"
   }
 
-  if(!phone){
+  if(!phone) {
     ee.phone = "Phone Number is required"
   }
 
-  if(!firstName || !lastName){
+  if(!firstName || !lastName) {
     ee.phone = "Enter at least the first or last name"
   }
 
@@ -174,7 +193,7 @@ function validate(state){
 }
 
 function validateEmail(email) {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
     return true
   }
   return false
